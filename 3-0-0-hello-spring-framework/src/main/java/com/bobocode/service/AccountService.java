e58dc39f26117461d21937e90d54dea6586f3708
user-MP <119966448+user-MP@users.noreply.github.com>
2023-05-08 18:45:47 +0300

    Add files via upload

---
+++ 3-0-0-hello-spring-framework/src/main/java/com/bobocode/service/AccountService.java
@@ -0,0 +1,33 @@
+package com.bobocode.service;
+
+import com.bobocode.dao.AccountDao;
+import com.bobocode.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static java.util.Comparator.comparing;
+
+/**
+ * {@link AccountService} is a very simple service that allows to find the richest person based on data provided to
+ * {@link AccountDao}.
+ * <p>
+ * Since it's a service that should be added to the application context, it is marked as Spring service. It order to get
+ * {@link AccountDao} instances, it uses implicit constructor-based injection.
+ */
+@Service
+public class AccountService {
+    private final AccountDao accountDao;
+
+
+
+    public AccountService(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public Account findRichestAccount() {
+        return accountDao.findAll()
+                .stream()
+                .max(comparing(Account::getBalance))
+                .orElseThrow();
+    }
+}
